853c05eefbe008f6134d7a87b4d326c0f0b8a375
Wilford <wilford@smartwebdesign.co.za>
2022-09-02 07:57:15 +0200

    added create view

--- mun_logistics/booking/urls.py
+++ mun_logistics/booking/urls.py
@@ -1,9 +1,10 @@
 # booking/urls.py
 from django.urls import path
-from .views import BookingListView, BookingDetailView
+from .views import BookingListView, BookingDetailView,BookingCreate
 
 
 urlpatterns = [
     path("booking/<int:pk>/",BookingDetailView.as_view(), name="booking_detail"),
+    path("bookingcreate/",BookingCreate.as_view(),name="create_booking"),
     path("", BookingListView.as_view(), name="home"),
 ]
--- mun_logistics/booking/views.py
+++ mun_logistics/booking/views.py
@@ -1,6 +1,8 @@
 # booking/views.py
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Booking_Details
+from django.urls import reverse_lazy
 class BookingListView(ListView):
     model = Booking_Details
     template_name = "home.html"
@@ -8,3 +10,9 @@ class BookingListView(ListView):
 class BookingDetailView(DetailView):
     model = Booking_Details   
     template_name = "booking_detail.html"
+
+class BookingCreate(CreateView):
+    model = Booking_Details
+    fields = "__all__"
+    template_name = "create_booking.html"
+    success_url = reverse_lazy("home")
